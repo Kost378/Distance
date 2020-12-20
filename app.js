159bbfdc25c3d73423ee67460e68ec22d9679fa7
Kost378 <kost97@inbox.ru>
2020-12-21 01:23:48 +0700

    js notification

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ let showMenu = function(){
 showMenu();
 
 let items = document.querySelectorAll('.right-icons a')
-console.log(items[2]);
 
 for(let i = 0; i < items.length - 1; i++){
   if(Math.round(Math.random())){
@@ -38,10 +37,6 @@ addRelations('like');
 addRelations('share');
 
 
-
-
-
-
 const nodeList = document.querySelectorAll("body *");
 
 function randomElementRandomBackground(){
@@ -58,3 +53,56 @@ function removeStyle(){
 }
 //setInterval(randomElementRandomBackground, 2000);
 //setInterval(removeStyle, 2000)
+
+rightIcons = document.querySelectorAll(".right-icons a");
+rightIcons.forEach(element => {
+ // element.preventDefault();
+  element.addEventListener("click", function(){
+    event.preventDefault();
+    this.classList.remove("notifications");
+    if(element.dataset.count){
+      ul = document.createElement("ul");
+      ul.style = "position: absolute; font-size: 12px; list-style-type: none; top: 50px; right: 5%; background-color: #fff; color: #000; padding: 10px; border-radius: 5px;";
+      let countNotification = +element.dataset.count > 10 ? 10: +element.dataset.count + 1;
+      for(let i = 1; i < countNotification; i++){
+        li = document.createElement("li");
+        li.innerText = `Уведомление номер ${i}`;
+        ul.appendChild(li);
+      }
+    
+        element.appendChild(ul);
+        element.dataset.count = '';
+    }
+    
+
+  })
+  element.addEventListener("blur", function(){
+    let ul = element.querySelector('ul');
+    if(ul){
+      ul.style = "display: none;"
+    }  
+  })
+
+ 
+});
+
+let create = document.querySelector("#create-post");
+create.addEventListener("click", function(){
+  event.preventDefault();
+  post = document.querySelector("section");
+
+  post2 = post.cloneNode(true);
+  document.querySelector("main").appendChild(post2);
+})
+let hideItems = document.querySelectorAll(".hide");
+setInterval(() => hideItems = document.querySelectorAll(".hide"), 2000);
+
+
+console.log(hideItems);
+hideItems.forEach(element => {
+  element.addEventListener("click", () => {
+    parent = element.closest("section");
+    parent.style.display = "none";
+  })
+});
+
